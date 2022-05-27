2a902cb63369aff1aaf61c18401413f82cb5139d
Tieli Cristine Lehnen <tielicristinesilva@gmail.com>
2022-05-27 19:40:45 -0300

    Aprimoramento de código

--- JAVA/ProjMaquinaDeLavar.java
+++ JAVA/ProjMaquinaDeLavar.java
@@ -4,7 +4,7 @@ public class ProjMaquinaDeLavar {
     static Scanner teclado = new Scanner(System.in);
 
     //variáveis utilizadas:
-    static int peso, c, codMenu, estagioInicial, duraEstagio1, duraEstagio2, duraEstagio3, duraEstagio4, duraEstagio5;
+    static int peso, c, codMenu, estagioInicial, duraEstagio1, tempo1, duraEstagio2, tempo2, duraEstagio3, tempo3, duraEstagio4, tempo4, duraEstagio5, tempo5;
     static String nome, modelo, volts;
 
     //Funções:
@@ -19,33 +19,50 @@ public class ProjMaquinaDeLavar {
         System.out.println("Digite o codigo:  ");
         codMenu = teclado.nextInt();
     }
-    public static void menuConfiguracao() {
+    public static void menuConfiguracao() throws InterruptedException {
+        new Thread().sleep(2000);
         System.out.println("===============================================================");
         System.out.println("=                CONFIGURACAO DA MAQUINA DE LAVAR             =");
         System.out.println("===============================================================");
         System.out.println();
+
         System.out.print("Modelo:  ");
         modelo = teclado.nextLine();
         modelo = teclado.nextLine();
+
         System.out.print("Nome:  ");
         nome = teclado.nextLine();
-        System.out.print("Voltagem (220V ou 110V ou 220/110V:  ");
+
+        System.out.print("Voltagem (220V ou 110V ou 220/110V):  ");
         volts = teclado.nextLine();
+
         System.out.print("Quantidade kgs:  ");
         peso = teclado.nextInt();
-        //Variável de duração de cada estágio para futuramente acrescentar tempo de processamento nos estágios:
+
+        //Variável de duração de tempo de processamento dos estágios:
+
         System.out.print("Duracao do Estagio 1 - Lavacao Pesada:  ");
         duraEstagio1 = teclado.nextInt();
+        tempo1 = duraEstagio1 * 1000;
+
         System.out.print("Duracao do Estagio 2 - Lavacao Normal:  ");
         duraEstagio2 = teclado.nextInt();
+        tempo2 = duraEstagio2 * 1000;
+
         System.out.print("Duracao do Estagio 3 - Lavacao Rapida:  ");
         duraEstagio3 = teclado.nextInt();
+        tempo3 = duraEstagio3 * 1000;
+
         System.out.print("Duracao do Estagio 4 - Enxague:  ");
         duraEstagio4 = teclado.nextInt();
+        tempo4 = duraEstagio4 * 1000;
+
         System.out.print("Duracao do Estagio 5 - Centrifuga:  ");
         duraEstagio5 = teclado.nextInt();
+        tempo5 = duraEstagio5 * 1000;
     }
-    public static void menuSimulacao() {
+    public static void menuSimulacao() throws InterruptedException {
+        new Thread().sleep(2000);
         System.out.println("===============================================================");
         System.out.println("=                 SIMULACAO DA MAQUINA DE LAVAR               =");
         System.out.println("===============================================================");
@@ -53,7 +70,8 @@ public class ProjMaquinaDeLavar {
         System.out.println("Simulando o Modelo:  " + modelo + " - " + nome + " - " + peso + " kg - Voltagem: " + volts);
         System.out.println();
     }
-    public static void menuEstagio() {
+    public static void menuEstagio() throws InterruptedException {
+        new Thread().sleep(2000);
         System.out.println("===============================================================");
         System.out.println("=                QUAL ESTAGIO VOCE DESEJA COMECAR?            =");
         System.out.println("===============================================================");
@@ -65,33 +83,38 @@ public class ProjMaquinaDeLavar {
         System.out.println("Selecione o estagio inicial: ");
         estagioInicial = teclado.nextInt();
     }
-    public static void estagio1() {
+    public static void estagio1() throws InterruptedException {
             for (c = 20; c <= 100; c += 20) {
                 System.out.println("Estagio 1 - Em Lavacao Pesada: " + c + " % ");
+                new Thread().sleep(tempo1);
             }
         }
-    public static void estagio2() {
+    public static void estagio2() throws InterruptedException {
             for (c = 20; c <= 100; c += 20) {
                 System.out.println("Estagio 2 - Em Lavacao Pesada: " + c + " % ");
+                new Thread().sleep(tempo2);
             }
         }
-    public static void estagio3() {
+    public static void estagio3() throws InterruptedException {
             for (c = 20; c <= 100; c += 20) {
                 System.out.println("Estagio 3 - Em Lavacao Pesada: " + c + " % ");
+                new Thread().sleep(tempo3);
             }
         }
-    public static void estagio4() {
+    public static void estagio4() throws InterruptedException {
             for (c = 20; c <= 100; c += 20) {
                 System.out.println("Estagio 4 - Em Lavacao Pesada: " + c + " % ");
+                new Thread().sleep(tempo4);
             }
         }
-    public static void estagio5() {
+    public static void estagio5() throws InterruptedException {
             for (c = 20; c <= 100; c += 20) {
                 System.out.println("Estagio 5 - Em Lavacao Pesada: " + c + " % ");
+                new Thread().sleep(tempo5);
             }
         }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner teclado = new Scanner(System.in);
 
         //Condicionamento do Menu Principal:
@@ -140,6 +163,7 @@ public class ProjMaquinaDeLavar {
                 codMenu = 0;
             } else if (codMenu == 2) {
                 System.out.println("Erro: Primeiro configure a maquina de lavar ");
+                new Thread().sleep(3000);
             }
         } while (codMenu != 0);
         System.out.println("Simulacao Finalizada");
