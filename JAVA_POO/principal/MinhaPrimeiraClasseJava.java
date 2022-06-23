9340b325700b86c9cff8a30bfd372244c473acf6
Tieli Cristine Lehnen <tielicristinesilva@gmail.com>
2022-06-22 21:06:48 -0300

    First commit

---
+++ JAVA_POO/principal/MinhaPrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package principal;
+
+public class MinhaPrimeiraClasseJava {
+    public static void main(String[] args) {
+        String texto = " turma";
+        System.out.println("Olá!" + texto);
+    }
+}
